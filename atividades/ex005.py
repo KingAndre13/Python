fb08507c03c89e6e1e4f5f2f960d2397b9494d1f
André Diogo <andrediogods@gmail.com>
2025-04-30 14:41:24 -0300

    Alguns ajustes

--- atividades/ex005.py
+++ atividades/ex005.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número: '))
-print('O número digitado foi: {}\nO seu dobro é {}\nO seu triplo é {}\nE sua raiz quadrada é {}'.format(n, n*2, n*3, n**(1/2)))
+print('O número digitado foi: {}\nO seu dobro é {}\nO seu triplo é {}\nE sua raiz quadrada é {:.2f}'.format(n, n*2, n*3, n**(1/2)))
